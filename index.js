91c7e41ba3267db552e46743e0c9ba58aa40417c
elifresh <45794105+elifresh@users.noreply.github.com>
2021-12-23 13:58:56 +0200

    ex3

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let num1 = prompt("Enter num 1");
 let num2 = prompt("Enter num 2")
-let sum = parseInt(num1)/parseInt(num2);
+let sum = Math.round( parseInt(num1)/parseInt(num2));
 
-alert("your number division"+' ' +math.round(sum))
+alert(`your number division ${sum}`);
